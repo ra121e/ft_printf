bcdc30c3e78d76104aacdabe1351ad466c7837b7
root <sushi.honda@gmail.com>
2024-06-07 00:18:31 +0800

    revise: Use clear variable name

--- ft_printfilter.c
+++ ft_printfilter.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 22:14:36 by athonda           #+#    #+#             */
-/*   Updated: 2024/06/06 23:08:12 by athonda          ###   ########.fr       */
+/*   Updated: 2024/06/07 00:15:12 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,28 +27,28 @@
 
 int	ft_printfilter(char c, va_list ap)
 {
-	size_t	i;
+	size_t	len;
 
-	i = 0;
+	len = 0;
 	if (c == 'c')
 	{
 		ft_putchar(va_arg(ap, int));
-		i = 1;
+		len = 1;
 	}
 	else if (c == 's')
-		i = ft_putstr(va_arg(ap, char *));
+		len = ft_putstr(va_arg(ap, char *));
 	else if (c == 'p')
-		i = ft_putptr(va_arg(ap, void *));
+		len = ft_putptr(va_arg(ap, void *));
 	else if (c == 'd' || c == 'i')
-		i = ft_putnbr(va_arg(ap, int));
+		len = ft_putnbr(va_arg(ap, int));
 	else if (c == 'u')
-		i = ft_putunbr(va_arg(ap, unsigned int));
+		len = ft_putunbr(va_arg(ap, unsigned int));
 	else if (c == 'x' || c == 'X')
 	{
 		if (c == 'x')
-			i = ft_puthexa(va_arg(ap, unsigned int), 0);
+			len = ft_puthexa(va_arg(ap, unsigned int), 0);
 		else
-			i = ft_puthexa(va_arg(ap, unsigned int), 1);
+			len = ft_puthexa(va_arg(ap, unsigned int), 1);
 	}
-	return (i);
+	return (len);
 }
