66f933dbbce431c12408f9ebae05a321a017a5a8
root <sushi.honda@gmail.com>
2024-06-06 12:10:56 +0800

    revise: function name, Add:Null check for ap

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 20:32:30 by athonda           #+#    #+#             */
-/*   Updated: 2024/06/06 01:04:35 by athonda          ###   ########.fr       */
+/*   Updated: 2024/06/06 09:28:45 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_filter(char c, va_list ap)
+int	ft_printfilter(char c, va_list ap)
 {
 	size_t	i;
 
@@ -57,6 +57,8 @@ int	ft_printf(const char *format, ...)
 
 	ptr = format;
 	va_start(ap, format);
+	if (ap == NULL)
+		return (-1);
 	index = 0;
 	count = 0;
 	i = 0;
@@ -64,7 +66,7 @@ int	ft_printf(const char *format, ...)
 	{
 		if (ptr[i] == '%')
 		{
-			index = index + ft_filter(ptr[i + 1], ap);
+			index = index + ft_printfilter(ptr[i + 1], ap);
 			i++;
 		}
 		else
