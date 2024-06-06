893bf2635064402f054735ff5186bed4a7bf6704
root <sushi.honda@gmail.com>
2024-06-07 01:09:43 +0800

    adjust: line limit 25

--- ft_printfilter.c
+++ ft_printfilter.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 22:14:36 by athonda           #+#    #+#             */
-/*   Updated: 2024/06/07 00:15:12 by athonda          ###   ########.fr       */
+/*   Updated: 2024/06/07 00:37:28 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,28 +27,27 @@
 
 int	ft_printfilter(char c, va_list ap)
 {
-	size_t	len;
-
-	len = 0;
 	if (c == 'c')
 	{
 		ft_putchar(va_arg(ap, int));
-		len = 1;
+		return (1);
 	}
 	else if (c == 's')
-		len = ft_putstr(va_arg(ap, char *));
+		return (ft_putstr(va_arg(ap, char *)));
 	else if (c == 'p')
-		len = ft_putptr(va_arg(ap, void *));
+		return (ft_putptr(va_arg(ap, void *)));
 	else if (c == 'd' || c == 'i')
-		len = ft_putnbr(va_arg(ap, int));
+		return (ft_putnbr(va_arg(ap, int)));
 	else if (c == 'u')
-		len = ft_putunbr(va_arg(ap, unsigned int));
-	else if (c == 'x' || c == 'X')
+		return (ft_putunbr(va_arg(ap, unsigned int)));
+	else if (c == 'x')
+		return (ft_puthexa(va_arg(ap, unsigned int), 0));
+	else if (c == 'X')
+		return (ft_puthexa(va_arg(ap, unsigned int), 1));
+	else if (c == '%')
 	{
-		if (c == 'x')
-			len = ft_puthexa(va_arg(ap, unsigned int), 0);
-		else
-			len = ft_puthexa(va_arg(ap, unsigned int), 1);
+		ft_putchar('%');
+		return (1);
 	}
-	return (len);
+	return (0);
 }
