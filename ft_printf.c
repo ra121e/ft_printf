9d7d7a95f097c1eaf5936363eec58e969a1f9c1c
root <sushi.honda@gmail.com>
2024-06-07 15:18:25 +0800

    Add comment

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,23 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 20:32:30 by athonda           #+#    #+#             */
-/*   Updated: 2024/06/07 15:02:17 by athonda          ###   ########.fr       */
+/*   Updated: 2024/06/07 15:17:49 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file ft_printf.c
+ * @brief To take several arguments and send va_list to ft_printfilter
+ */
+
 #include "ft_printf.h"
+
+/**
+ * @fn int	ft_printf(const char *format, ...)
+ * @brief genarate argument point by va_ series macros, and detect %
+ * @param[in]	format	char pointer for the static argument
+ * @return 		length of the total text after converting specifier to arges
+ */
 
 int	ft_printf(const char *format, ...)
 {
